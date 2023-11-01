8003e81b252bba46c3be90e8678ac7a2bc74eda4
Leone Varando <lvarando@gmail.com>
2023-10-31 19:03:07 -0500

    Added functional seed file

--- db/seed.js
+++ db/seed.js
@@ -5,91 +5,78 @@ const { db } = require('./connection')
 const userData = require('./data/demoUserData.json')
 
 const seedSync = async () => {
+  console.log('Loading...')
   let mlsId = 253;
   const seasonId = 2023
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': process.env.TOKEN,
+      'X-RapidAPI-Key': "6a563d7cf3msh4c701a8a48ee5fcp122fa8jsne2418614c152",
       'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
     }
   };
+  const playerData = [];
 
-  const playersStats = [];
+  let url = `https://api-football-v1.p.rapidapi.com/v3/players?league=${mlsId}&season=${seasonId}&page=1`;
+  const fetchVar = await fetch(url, options);
+  const playerInfo = await fetchVar.json();
+  const playerInfoRes = await playerInfo.response
+  let totalPage = playerInfo.paging.total
 
-  try {
-    console.log('Loading...')
-    const teamsUrl = `https://api-football-v1.p.rapidapi.com/v3/teams?league=${mlsId}&season=${seasonId}`;
-    const teams = await fetch(teamsUrl, options);
-    const teamResult = await teams.json();
-    const teamInfo = await teamResult.response;
-    const teamIds = [];
-  
-    for (let i = 0; i < teamInfo.length; i++) {
-      teamIds.push(teamInfo[i].team.id);
+  for (let i = 0; i < playerInfoRes.length; i++) {
+    const player = playerInfoRes[i].player
+    const stats = playerInfoRes[i].statistics[0]
+    const playObj = {
+      apiId: player.id,
+      firstName: player.firstname,
+      lastName: player.lastname,
+      displayName: player.name,
+      age: player.age,
+      img: player.photo,
+      nationality: player.nationality,
+      team: stats.team.name,
+      height: player.height,
+      weight: player.weight,
+      injured: player.injured,
+      position: stats.games.position
     }
-
-    let page = 1;
-
-    for (let i = 0; i < teamIds.length; i++) {
-      // Trying to grab player positions and numbers
-      const squadUrl = `https://api-football-v1.p.rapidapi.com/v3/players/squads?team=${teamIds[i]}`;
-      const squad = await fetch(squadUrl, options);
-      const squadResult = await squad.json();
-      const squadRes = squadResult.response[0];
-
-      for (let j = 0; j < squadRes.players.length; j++) {
-        playersStats.push(squadRes.players[j]);
-      }
-    }
-
-    let totalPlayerInfo = [];
-
-    for (let i = 0; i < teamIds.length; i++) {
-      const playersUrl = `https://api-football-v1.p.rapidapi.com/v3/players?team=${teamIds[i]}&season=${seasonId}&page=${page}`;
-      const players = await fetch(playersUrl, options);
-      const playersResult = await players.json();
-      const playerInfo = await playersResult.response;
-
-      if (page < playersResult.paging.total) {
-        page++;
-        i--;
-      } else if (playersResult.paging.current === playersResult.paging.total) page = 1;
-
-      for (let j = 0; j < playerInfo.length; j++) {
-        totalPlayerInfo.push(playerInfo[j]);
-      }
-    }
-
-    playersStats.sort((a,b) => a.id - b.id);
-    totalPlayerInfo.sort((a,b) => a.player.id - b.player.id);
-
-    for (let i = 0, j = 0; i < playersStats.length; i++, j++) {
-      const objectInsertion = playersStats[i]
-      const curPlayerInfo = totalPlayerInfo[j].player;
-      const curPlayerStats = totalPlayerInfo[j].statistics[0];
-      if (objectInsertion.id === curPlayerInfo.id) {
-        objectInsertion.firstName = curPlayerInfo.firstname;
-        objectInsertion.lastName = curPlayerInfo.lastname;
-        objectInsertion.nationality = curPlayerInfo.nationality;
-        objectInsertion.team = curPlayerStats.team.name;
-        objectInsertion.height = curPlayerInfo.height;
-        objectInsertion.weight = curPlayerInfo.weight;
-        objectInsertion.injured = curPlayerInfo.injured;
-      } else {
-        i--;
-      }
-    }
-  } catch (e) {
-      console.error(e);
+    playerData.push(playObj);
   }
-  console.log(playersStats)
+
+  for (let i = 2; i <= totalPage; i++) {
+    url = `https://api-football-v1.p.rapidapi.com/v3/players?league=${mlsId}&season=${seasonId}&page=${i}`;
+    const f = await fetch(url, options);
+    const loopPlayer = await f.json();
+    const res = await loopPlayer.response;
+
+    for (let j = 0; j < res.length; j++) {
+      const player = res[j].player;
+      const stats = res[j].statistics[0];
+      const playObj = {
+        apiId: player.id,
+        firstName: player.firstname,
+        lastName: player.lastname,
+        displayName: player.name,
+        age: player.age,
+        img: player.photo,
+        nationality: player.nationality,
+        team: stats.team.name,
+        height: player.height,
+        weight: player.weight,
+        injured: player.injured,
+        position: stats.games.position
+      }
+      playerData.push(playObj);
+    }
+  }
+  
+  console.log(playerData)
   await db.sync({ force: true })
-  await Player.bulkCreate(playersStats)
+  await Player.bulkCreate(playerData)
   await User.bulkCreate(userData.users)
   console.log('Seed has been planted')
 }
 
-// seedDb()
+seedSync()
 
 module.exports = seedSync;
